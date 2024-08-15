b70c269e817e793d4af4f7b8f925b675e78026e8
kenshgy <ken.shgy.1386@gmail.com>
2024-08-16 00:28:15 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
     vueJsx(),
