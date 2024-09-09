20db05b89eca6ec28033658279c26fa5cc3d6772
kenshgy <ken.shgy.1386@gmail.com>
2024-09-10 03:10:50 +0900

    パスパラメーターでシート名を指定可能に

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,13 +1,16 @@
 import http from '../http-common'
 
+// 下記のスプレッドシートのデータを取得する
+// https://docs.google.com/spreadsheets/d/1XBUGCAwoNopXpmxZ1bAN5eofScOlkcV_riqpUTgHRQo
 const url =
  'https://script.google.com/macros/s/AKfycbwxZm06fLzdMqtM3wZIoeE89LpdpiUQ1DArgH248BtHL8kUBHDrivVpqyn4sZ_8-Q0M/exec'
-const params = {
- crossDomain: true,
- sheetName: 'demo'
-}
+
 class GetData {
- async get() {
+ async get(sheetName: string) {
+  const params = {
+   crossDomain: true,
+   sheetName: sheetName ? sheetName : 'demo'
+  }
   const result = await http.get(url, { params })
   return result.data
  }
