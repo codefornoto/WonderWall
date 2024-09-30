0838c1b89ec1b63c857f6cff0f793680645fe11a
kenshgy <ken.shgy.1386@gmail.com>
2024-09-30 10:27:16 +0900

    画像表示対応

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: '/WonderWall',
+ define: {
+  'process.env': {}
+ },
+ base: process.env.GITHUB_PAGES ? '/WonderWall/' : './',
  build: {
   outDir: 'docs'
  },
